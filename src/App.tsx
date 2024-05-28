945386375a201f83baee7992a1f42e57718b9b41
asadullah <ullahasad610@gmail.com>
2024-05-28 12:59:30 +0500

    Initialized fabric js Canvas and  add rect to canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-
-import { useRef } from 'react'
+import { Canvas, Rect } from 'fabric';
+import { useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
-  const canvasRef = useRef(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const canvasInitialized = useRef<boolean>(false);
+
+  
+
+  useEffect(() => {
+    if(!canvasRef.current || canvasInitialized.current) return;
+
+    const canvas = new Canvas(canvasRef.current, {height: 800, width: 1000, backgroundColor: 'white'});
+    canvas.renderAll();
+    const rect = new Rect({left: 100, top: 100, fill: 'red', width: 100, height: 100});
+    canvas.add(rect);
+    console.log(canvas);
+    canvasInitialized.current = true;
+    //@ts-expect-error-2339
+    window.fabCanvas = canvas;
+  }, []);
 
   return (
-    <>
-      <h1>Vite + React + FabricV6 with help of Yarn</h1>
+    <div className={'flex'}>
+      <h1 className=''>Fabric v6</h1>
 
       <canvas ref={canvasRef}></canvas>
-    </>
+    </div>
   )
 }
 
